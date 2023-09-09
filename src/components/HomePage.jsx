7e7373c76fcfe9ad83fe3e809ad8c24653b424db
AnaMigriauli <anamigriauli1994@gmail.com>
2023-09-09 21:15:37 +0200

    add home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,31 @@
 import backgroundImage from "../assets/images/martin-katler-o9XN28KdyN8-unsplash.jpg";
+import linkedin from "../assets/images/linkedin.svg";
+import github from "../assets/images/github.svg";
+import mail from "../assets/images/mail.svg";
+import classes from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <div
+      className={classes.homePage}
       style={{
         backgroundImage: `url(${backgroundImage})`,
-        width: "382px",
-        height: "681px",
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
       }}
-    ></div>
+    >
+      <h1 className={classes.greeting}>Hi, I am</h1>
+      <p className={classes.userName}>Ana Migriauli</p>
+      <p className={classes.userRole}>Junior React developer</p>
+      <div className={classes.contactLinks}>
+        <a className={classes.mailLink}>
+          <img src={mail} alt="mail" />
+        </a>
+        <a className={classes.githubLink}>
+          <img src={github} alt="GitHub" />
+        </a>
+        <a className={classes.linkedinLink}>
+          <img src={linkedin} alt="LinkedIn" />
+        </a>
+      </div>
+    </div>
   );
 };
 export default HomePage;
